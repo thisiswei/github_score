e0b85d0ac12697bf96b4a05f7eacb6449ae779b0
thisiswei <99plus2@gmail.com>
2012-11-27 19:33:23 -0800

    'build Github module to fetch json,and parse it'

---
+++ lib/client.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'digest/md5'
+require 'open-uri'
+
+module Github
+  class Client
+    
+    def self.fetch username
+      new.fetch username
+    end
+
+    def fetch username
+      records = JSON.parse get username
+      records.each do |hash|
+        uniq_id = Digest::MD5.hexdigest Marshal.dump(hash)
+        hash[uniq_id] = uniq_id
+      end
+      records
+    end
+    
+
+    private
+      def get username
+        open("https://github.com/#{username}.json").read
+      end
+   
+  end
+
+end
