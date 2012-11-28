b8e4cd49a94b8d6f4ebca2d7fce70567003d4fc7
thisiswei <99plus2@gmail.com>
2012-11-27 18:11:19 -0800

    loading...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubScore::Application.config.secret_token = '546c4aabeefafcd08f5b18bd96fd72fad5737628820303eae9af0fd54154d7431eeb2a04ca2471e3b01d3676b36dea79969dee2e85050cb43df59e145f5984cd'
